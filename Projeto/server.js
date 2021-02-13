67c6a5f53b6d427d9f137c217eaf5991aedf8d91
Maisa <maisa.ssa@gmail.com>
2021-02-13 20:37:28 -0300

    Projeto - Prototipo inicial

---
+++ Projeto/server.js
@@ -0,0 +1,14 @@
+//REQUISIÇÃO DOS MÓDULOS:
+
+/*Express*/const express = require('express');
+/*Express*/const app = express()
+
+//ROTAS 
+//Express.static: entrega arquivos estáticos da pasta client
+
+app.use('/cliente', express.static('client'))
+
+
+app.listen(8083, function(){
+    console.log("Servidor rodando na URL http://localhost:8083")
+})
